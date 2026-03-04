c7e765e767077febe2353179327a99ffbc064617
Flabio Adrian Lucas Martinez <20240984@uthh.edu.mx>
2026-03-04 11:31:39 -0600

    Update environment config and CORS options

--- src/app.js
+++ src/app.js
@@ -11,8 +11,10 @@ import reservasRoutes from './routes/reservas.routes.js';
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH' ],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.use(express.json());
